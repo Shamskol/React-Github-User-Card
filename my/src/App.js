6f143296c1f34cc541a7b66b93168abf2f285195
Kolade-Junaid <koladejun@yahoo.com>
2020-02-11 22:49:14 +0000

    Fetched the user data, displayedthe user data and fetched the user's followers

--- my/src/App.js
+++ my/src/App.js
@@ -1,14 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GitHubUser  from "./GitHubUser";
+import React from "react";
+import axios from "axios";
+import GitHubUser from "./Components/GitHubUser";
 
-function App() {
-  return (
-    <div>
-      <GitHubUser/>
-    </div>
-  );
+
+class App extends React.Component {
+
+ constructor(props) {
+        super(props);
+this.state = {
+    data: [], 
+    followers: [],
+ 
+};
+
+    }
+
+
+componentDidMount() {
+ axios.get("https://api.github.com/users/Shamskol")
+
+  .then (response => {
+ 
+  this.setState({data:response.data});
+  console.log(this.state.data);
+  console.log(response.data);
+      }) 
+      .catch(error => {
+          console.log(error);
+
+      });
+
+      axios.get("https://api.github.com/users/Shamskol/followers")
+
+      .then(res => {
+        this.setState({ followers: res.data })
+        console.log(this.state.followers)
+      })
+      .catch(err => {
+      
+        alert(err.response.data.message)
+      })
+
+        
+        
+    }
+render(){
+
+  return(
+<div>
+
+<GitHubUser
+
+data = {this.state.data}
+
+
+
+/>
+<div>
+  {this.state.followers.map( follower=>{
+return (<div>
+   <img src={follower.avatar_url} alt="github photo"/>
+  <p> UserName: 
+    {follower.login  }
+  </p>
+  <p>
+   id :
+  {follower.id} 
+  
+  </p>
+ 
+  <p>
+   email: {follower.email} 
+  </p>
+  <p>
+   Following: {follower.following} 
+  </p>
+  <p>
+   Followers: {follower.followers} 
+  </p> 
+  </div>)
+
+  })}
+  
+</div>
+
+</div>
+
+
+  )
 }
 
+  }
+
+
+
+
 export default App;
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
